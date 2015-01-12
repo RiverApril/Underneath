d751ad7090808e2bc050eef39a71f87b54b2f8f1
Braeden Atlee <nedearb@gmail.com>
2015-01-12 00:54:41 -0700

    Fixed a -> that should be . in a comment
    Lol, this must have happened when I did a find and replace.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 //  Underneath
 //
 //  Created by Braeden Atlee on 9/29/14.
-//  Copyright (c) 2014 Braeden Atlee. All rights reserved->
+//  Copyright (c) 2014 Braeden Atlee. All rights reserved.
 //
 
 #include "Command.h"
