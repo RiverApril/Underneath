9b97dd157cbf26e35040f7d4a55196266d774099
Nedearb <nedearb@gmail.com>
2016-02-15 17:26:58 -0700

    icon and app

--- makeApp.sh
+++ makeApp.sh
@@ -3,11 +3,11 @@ rm -rf $@.app
 mkdir -p $@.app
 mkdir -p $@.app/lib
 mkdir -p $@.app/MacOS
-mkdir -p $@.app/Recources
+mkdir -p $@.app/Resources
 cp $@ $@.app/MacOS/
-cp font.png $@.app/MacOS/
-cp -r audio $@.app/MacOS/
-cp icon.icns $@.app/Recources/
+cp font.png $@.app/Resources/
+cp -r audio $@.app/Resources/
+cp icon.icns $@.app/Resources/
 chmod a+x $@.app/MacOS/$@
 otool -L $@ | awk '{if(NR>1)print}' | while read -r line ; do
     file=${line%%'('*}
@@ -26,6 +26,8 @@ text="
   <string>icon.icns</string>
   <key>CFBundleExecutable</key>
   <string>$@</string>
+  <key>WorkingDirectory</key>
+  <string>\$APP_PACKAGE/Resources</string>
 </dict>
 </plist>
 "
